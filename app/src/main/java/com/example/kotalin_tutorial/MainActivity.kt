c954d234a66b9e72e3098ebcf942d8d9c789f6c1
devpatel2784 <106700027+devpatel2784@users.noreply.github.com>
2022-06-14 13:09:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/kotalin_tutorial/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.kotalin_tutorial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton :Button = findViewById (R.id.btn_hyy)
+        rollButton.setOnClickListener {
+            rollDisc()
+        }
+    }
+
+    private fun rollDisc() {
+
+        val drawableResours = when (Random().nextInt(6)+1)
+        {
+            1 ->R.drawable.dice_one
+            2 ->R.drawable.dice_two
+            3 ->R.drawable.dice_three
+            4 ->R.drawable.dice_four
+            5 ->R.drawable.dice_five
+            else ->R.drawable.dice_six
+
+        }
+
+        val diceImage :ImageView = findViewById(R.id.image_dice)
+        diceImage.setImageResource(drawableResours)
+
+
+
+
+    }
+}
